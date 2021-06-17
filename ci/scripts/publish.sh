3512d1bec89fef607f626ca60c3b9ff5f88f10a8
Ulrich <12871773+devulrix@users.noreply.github.com>
2021-06-17 13:45:31 +0200

    fix replication

--- ci/scripts/publish.sh
+++ ci/scripts/publish.sh
@@ -4,4 +4,4 @@ set -ue
 
 #connect to ftp 
 
-lftp -c "open  -u $FTP_USERNAME,$FTP_PASSWORD $FTP_SERVER; mirror -R blog-deployment /"
+lftp -c "open  -u $FTP_USERNAME,$FTP_PASSWORD $FTP_SERVER; mirror -e -R blog-deployment /"
