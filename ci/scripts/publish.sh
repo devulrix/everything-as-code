a8230c37b6ca2b725ef6518b0905d1d74c877a56
Ulrich <12871773+devulrix@users.noreply.github.com>
2021-06-22 16:46:14 +0200

    fix missing dot files

--- ci/scripts/publish.sh
+++ ci/scripts/publish.sh
@@ -4,4 +4,4 @@ set -ue
 
 #connect to ftp 
 
-lftp -c "open  -u $FTP_USERNAME,$FTP_PASSWORD $FTP_SERVER; mirror -e -R blog-deployment /"
+lftp -c "open  -u $FTP_USERNAME,$FTP_PASSWORD $FTP_SERVER -e 'set ftp:list-options -a'; mirror -e -R blog-deployment /"
